1899e4f2f47a365c373f0970bbb44a1a80e9f486
GromShine <GromShine@users.noreply.github.com>
2022-10-21 22:15:17 +0800

    update

--- utils.py
+++ utils.py
@@ -177,13 +177,15 @@ def forward_pass(func, z0, tspan, dt, batch, evnt_align, A_matrix, gs_info=None,
     # input: t_total*county_num*(n1+n2)
     # output: t_total*couty_num*dim_N,每个维度,每个时间节点上、每个事件类型的lambda
     params = func.L(trace)
+    print(params.size())
     print(len(params[0]))
-    exit
+   
     # print(lmbda.size())
     # torch.Size([1815, 12, 12])
     # 取出了事件类型总数dim_N的lambda
     lmbda = params[..., :func.dim_N]
-
+    print(lmbda.size())
+    exit
     if gs_info is not None:
         lmbda[:, :, :] = torch.tensor(gs_info[0])
 
